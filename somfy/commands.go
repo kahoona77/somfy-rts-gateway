79a364cf7e66289ae53b721cc4adbc7687748e55
benjamin.ernst <benjamin@kuoo.de>
2020-10-21 21:43:32 +0200

    add prog

--- somfy/commands.go
+++ somfy/commands.go
@@ -7,6 +7,7 @@ type Button uint16
 const ButtonMy = 0x1
 const ButtonUp = 0x2
 const ButtonDown = 0x4
+const ButtonProg = 0x8
 
 func getButtonFromCommand(cmd string) (Button, error) {
 	switch cmd {
@@ -16,6 +17,8 @@ func getButtonFromCommand(cmd string) (Button, error) {
 		return ButtonDown, nil
 	case "my":
 		return ButtonMy, nil
+	case "prog":
+		return ButtonProg, nil
 	}
 	return ButtonMy, fmt.Errorf("could not map command %s", cmd)
 }
