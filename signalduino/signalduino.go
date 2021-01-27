21afa4bdae6c2419912a0faae48944e8e4e082d6
bernst <bernst@newyorker.de>
2021-01-27 15:18:57 +0100

    debug

--- signalduino/signalduino.go
+++ signalduino/signalduino.go
@@ -20,7 +20,7 @@ func Open(devicePort string) (*Signalduino, error) {
 		DataBits:              8,
 		StopBits:              1,
 		ParityMode:            0,
-		RTSCTSFlowControl:     false,
+		RTSCTSFlowControl:     true,
 		InterCharacterTimeout: 0,
 		MinimumReadSize:       4,
 	}
